9feb6c926faafe9250b89f7adaf3c926bff85fd6
inifawaz <inifawaz@outlook.co.id>
2023-02-01 14:25:04 +0700

    sdfkjhcxvkjhcv

--- src/components/ApplicationLogo.js
+++ src/components/ApplicationLogo.js
@@ -5,6 +5,7 @@ const ApplicationLogo = props => (
 
     <div className="h-12 w-40 relative">
         <Image alt="Isalam Logo" src={'/isalam-light.png'} sizes={40} priority fill className="object-contain" />
+
     </div>
 )
 
